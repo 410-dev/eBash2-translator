bfbd23f28c0c79d0d686ae76fe3ad49d3728c31e
410 <hysong0129@gmail.com>
2022-04-14 14:53:48 +0900

    Will show build result

--- src/eb2translator/Main.java
+++ src/eb2translator/Main.java
@@ -5,12 +5,20 @@ import eb2translator.ebash2.runtime.Runtime;
 
 public class Main {
 
+    private static int warnings = 0;
+    private static long msec = 0;
+
     public static String ebash2path = "/usr/local/eBash2";
+
+    public static String currentClass = "";
 
     public static void main(String[] args) throws Exception {
         System.out.println("eBash2 to Native Bash Translator");
         System.out.println("Copyright (C) 2021 _410");
         System.out.println("");
+
+        // Time measure start
+        long start = System.currentTimeMillis();
 
         // Check if there is eBash runtime
         Runtime.checkRuntime(args);
@@ -29,6 +37,13 @@ public class Main {
 
         // Copy non-compilable files
         FileUtils.copyNonCompilableFiles();
+
+        // Time measure end
+        long end = System.currentTimeMillis();
+        msec = end - start;
+
+        // Build success
+        printBuildSuccess();
     }
 
 
@@ -46,4 +61,45 @@ public class Main {
         System.out.println(s);
     }
 
+    private static void printBuildSuccess() {
+        String msg = "                                  " + "\n" +
+"                               &@@@@@@@@@@@@(               " + "\n" +
+"                                      %@@@@@@@@@            " + "\n" +
+"                                        #@@@@@@@@@          " + "\n" +
+"                                       @@@@@@@@@@@@@        " + "\n" +
+"                                    ( @@@@@@@@@@@@@@(       " + "\n" +
+"                                  @@@@@ .@@@@   ,@@@@@@@,   " + "\n" +
+"                                @@@@@@@@@@        .@@@@@@   " + "\n" +
+"                             ,@@@@@@@@@             (@/     " + "\n" +
+"                           @@@@@@@@@#                       " + "\n" +
+"                        ,@@@@@@@@@                          " + "\n" +
+"                      @@@@@@@@@*                            " + "\n" +
+"                   #@@@@@@@@@                               " + "\n" +
+"                .@@@@@@@@@/                                 " + "\n" +
+"              @@@@@@@@@@                                    " + "\n" +
+"           @@@@@@@@@@@                                      " + "\n" +
+"        @@@@@@@@@@@#                                        " + "\n" +
+"     %@@@@@@@@@@@                                           " + "\n" +
+"      @@@@@@@@@                                             " + "\n" +
+"        @@@@@                                               ";
+
+        String msg2 = "                                  " + "\n" +
+"______       _ _     _                                  "+ "\n" +
+"| ___ \\     (_) |   | |                                  "+ "\n" +
+"| |_/ /_   _ _| | __| |  ___ _   _  ___ ___ ___  ___ ___ "+ "\n" +
+"| ___ \\ | | | | |/ _` | / __| | | |/ __/ __/ _ \\/ __/ __|"+ "\n" +
+"| |_/ / |_| | | | (_| | \\__ \\ |_| | (_| (_|  __/\\__ \\__ \\"+ "\n" +
+"\\____/ \\__,_|_|_|\\__,_| |___/\\__,_|\\___\\___\\___||___/___/"+ "\n";
+        System.out.println(msg + "\n" + msg2);
+
+
+        System.out.println("Program build succeeded with " + warnings + " warnings.");
+        System.out.println("Build time: " + msec + " ms (" + convertToMinutes(msec) + ")");
+    }
+
+    private static String convertToMinutes(long msec) {
+        long minutes = msec / 60000;
+        long seconds = (msec % 60000) / 1000;
+        return minutes + "m " + seconds + " s";
+    }
 }
